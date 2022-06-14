9b3af612c814502761ae43c3986d21c0317f1a9a
f3zinker <106789797+f3zinker@users.noreply.github.com>
2022-06-14 16:45:21 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
-import glob
 import os
+import glob
 import pandas as pd
 from psaw import PushshiftAPI
 from datetime import datetime as dt
@@ -14,7 +14,7 @@ api = PushshiftAPI()
 while start_epoch < end_epoch + 86200: 
     # Starting time string formatting
     start_date = dt.fromtimestamp(start_epoch)
-    dateStr = start_date.strftime("%Y %b %d")
+    date_str = start_date.strftime("%Y %b %d")
 
     # Make request
     api_request_generator = api.search_comments(subreddit='redscarepod',
@@ -24,13 +24,13 @@ while start_epoch < end_epoch + 86200:
     
     try:
         print(f"Beggining to process data for {dateStr}")
-        missy_comments = pd.DataFrame([comment.d_ for comment in api_request_generator])
-        missy_comments['date'] = pd.to_datetime(missy_comments['created_utc'], utc=True, unit='s')
-        missy_central = missy_comments[['date','score','body']]
-        print(f"Data processed for {dateStr}")
-        missy_central.to_csv(r"data\\" + dateStr + "_.csv", index = False, header = True)
+        df_comments = pd.DataFrame([comment.d_ for comment in api_request_generator])
+        df_comments['date'] = pd.to_datetime(missy_comments['created_utc'], utc=True, unit='s')
+        df_central = df_comments[['date','score','body']]
+        print(f"Data processed for {date_str}")
+        missy_central.to_csv(fr"data\\{date_str}.csv", index = False, header = True)
     except KeyError:
-        print(f"Got key error skipping {dateStr}")
+        print(f"Got key error skipping {date_str}")
         pass
         
     start_epoch += 860000//2 # 4 days
